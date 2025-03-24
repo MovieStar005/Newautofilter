3d07560206bb1b247e51f79fafde3cc557602cb8
MovieStar005 <152777826+MovieStar005@users.noreply.github.com>
2025-03-24 10:00:43 +0530

    Update info.py

--- info.py
+++ info.py
@@ -61,8 +61,8 @@ HOW_TO_VERIFY = environ.get('HOW_TO_VERIFY', 'https://t.me/All_Movie_Star_Link')
 
 # Shortner 
 IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', True))
-SHORTLINK_URL = environ.get('SHORTLINK_URL', 'shortner.in')
-SHORTLINK_API = environ.get('SHORTLINK_API', 'eb10aca9184c1b20e4ac653f41566b8ca775b85d')
+SHORTLINK_URL = environ.get('SHORTLINK_URL', 'urlwala.com')
+SHORTLINK_API = environ.get('SHORTLINK_API', 'd8835920f4453699343a04fd3b116e50c0e54221')
 TUTORIAL = environ.get('TUTORIAL', 'https://t.me/All_Movie_Star_Link') # Tutorial video link for opening shortlink website 
 IS_TUTORIAL = bool(environ.get('IS_TUTORIAL', False))
 
