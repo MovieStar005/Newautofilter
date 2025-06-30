16a3236bd0faf439856da4d090258d2935127ff8
MovieStar005 <152777826+MovieStar005@users.noreply.github.com>
2025-06-30 18:46:59 +0530

    Update info.py

--- info.py
+++ info.py
@@ -37,8 +37,8 @@ DEENDAYAL_MOVIE_UPDATE_CHANNEL = int(environ.get('DEENDAYAL_MOVIE_UPDATE_CHANNEL
 PREMIUM_LOGS = int(environ.get('PREMIUM_LOGS', '-1002096756735')) # add premium logs channel id
 auth_channel = environ.get('AUTH_CHANNEL', '-1002838261214') #Channel / Group Id for force sub ( make sure bot is admin )
 DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in environ.get('DELETE_CHANNELS', '-1002096756735').split()]
-support_chat_id = environ.get('SUPPORT_CHAT_ID', '-1002122467397') # support group id ( make sure bot is admin )
-reqst_channel = environ.get('REQST_CHANNEL_ID', '-1002219302448') # request channel id ( make sure bot is admin ).
+support_chat_id = environ.get('SUPPORT_CHAT_ID', '-1002714431620') # support group id ( make sure bot is admin )
+reqst_channel = environ.get('REQST_CHANNEL_ID', '-1002850264292') # request channel id ( make sure bot is admin ).
 
 
 # MongoDB information
